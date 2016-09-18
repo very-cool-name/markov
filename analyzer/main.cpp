4efdeadfe20c3c7d4ec4b3ae81b058fc596c1b03
DoctorMoisha <vigorous.activity@gmail.com>
2016-09-18 22:34:06 +0300

    Serialization and deserialization with stupid pointers

--- analyzer/main.cpp
+++ analyzer/main.cpp
@@ -2,13 +2,24 @@
 #include <iostream>
 #include <fstream>
 
-#include "cereal/archives/json.hpp"
+#include <cereal/archives/json.hpp>
 
-#include "markov_chain/markov.hpp"
+#include <markov_chain/markov.hpp>
 
 int main() {
-  std::fstream in("input.txt");
-  markov::Chain chain(in);
-  cereal::JSONOutputArchive output(std::cout);
-  output(chain);  
+  {
+    std::fstream in("input.txt");
+    std::fstream out("output.txt");
+    markov::Chain chain(in);
+    cereal::JSONOutputArchive output(out);
+    output(chain);
+  }
+  markov::Chain serialized;
+  std::ifstream in_arc("output.txt");
+  serialized.SerializeFrom<cereal::JSONInputArchive>(in_arc);
+  {
+    std::ofstream out("output2.txt");
+    cereal::JSONOutputArchive output(out);
+    output(serialized);
+  }
 }
--- include/markov_chain/markov.hpp
+++ include/markov_chain/markov.hpp
@@ -20,17 +20,22 @@ namespace markov {
     const StrType* word;
     // WordWithFrequency is used in unorder_map
     mutable int frequency;
-
-    template<class Archive>
-    void save(Archive& archive) const {
-      archive(*word, frequency);
-    }
-
-    template<class Archive>
-    void load(Archive& archive, markov::WordWithFrequency& chain) {
-      throw std::runtime_error("Not okay");
-    }
   };
+
+  template<class Archive>
+  void save(Archive& archive, const WordWithFrequency& inst) {
+    archive(cereal::make_nvp("word", *inst.word));
+    archive(cereal::make_nvp("frequency", inst.frequency));
+  }
+
+  template<class Archive>
+  void load(Archive& archive, WordWithFrequency& chain) {
+    std::string word;
+    archive(cereal::make_nvp("word", word));
+    archive(cereal::make_nvp("frequency", chain.frequency));
+    auto in_tokens = static_cast<ArchiveAndTokens<Archive>&>(archive).tokens.insert(word);
+    chain.word = &(*in_tokens.first);
+  }
 
   bool operator==(const WordWithFrequency& lhs, const WordWithFrequency& rhs) {
     return *lhs.word == *rhs.word;
@@ -85,23 +90,36 @@ namespace markov {
       }
     }
 
-    template<class Archive>
+   private: 
+    /*template<class Archive>
     void save(Archive& archive) const {
-      archive(freqs_);
+      archive(cereal::make_nvp("map", freqs_));
     }
 
     template<class Archive>
     void load(Archive& archive) {
-      // archive(inst.freqs_);
+      throw std::runtime_error("Not okay");
+    }*/
+
+    template<class Archive>
+    void serialize(Archive& archive) {
+      archive(cereal::make_nvp("map", freqs_));
     }
 
-   private: 
     FrequencyMap freqs_;
   };
 
+  template <class Archive>
+  struct ArchiveAndTokens : public Archive {
+    ArchiveAndTokens(std::istream& stream, 
+                     std::unordered_set<StrType>& in_tokens) : Archive(stream), tokens(in_tokens) {}
+    std::unordered_set<StrType>& tokens;
+  };
+
   class Chain {
-    friend class cereal::access;
    public:
+    Chain() {}
+
     Chain(std::istream& in, int depth = 3)
       : depth_(depth)
     {
@@ -130,56 +148,57 @@ namespace markov {
     }
 
     template<class Archive>
+    void SerializeFrom(std::istream& stream) {
+      ArchiveAndTokens<Archive> ar(stream, tokens_);
+      ar(*this);
+    }
+
+   private:
+    friend class cereal::access;
+
+    /*template<class Archive>
     void save(Archive& archive) const {
-      archive(head_);
+      archive(cereal::make_nvp("head", head_));
     }
 
     template<class Archive>
     void load(Archive& archive) {
-      // archive(inst.freqs_);
+      throw std::runtime_error("Not okay");
+    }*/
+
+    template<class Archive>
+    void serialize(Archive& archive) {
+      archive(cereal::make_nvp("depth", depth_));
+      archive(cereal::make_nvp("head", head_));
     }
 
-   private:
     std::unordered_set<StrType> tokens_;
     WordsFrequency head_;
     int depth_;
   };
 } // namespace markov
-
-
 /*
-template<class Archive>
-inline void save(Archive& archive, const markov::Chain& inst) {
-  archive(inst.head_);
-}
+namespace cereal {
+  //! Saving std::unique_ptr for non polymorphic types
+  template <class Archive, class Deleter> inline
+  void 
+  CEREAL_SAVE_FUNCTION_NAME(Archive & ar, const std::unique_ptr<markov::WordsFrequency, Deleter>& ptr) {
+    if (ptr)
+      ar(*ptr);
+  }
 
-template<class Archive>
-inline void save(Archive& archive, const markov::WordsFrequency& inst) {
-  // for (auto& word : inst.freqs_) {
-  archive(inst.freqs_);
-  //}
+  //! Loading std::unique_ptr for non polymorphic types
+  template <class Archive, class Deleter> inline
+  void
+  CEREAL_LOAD_FUNCTION_NAME(Archive & ar, std::unique_ptr<markov::WordsFrequency, Deleter>& ptr) {
+    try {
+      markov::WordsFrequency freqs;
+      ar(cereal::make_nvp("map", freqs));
+      ptr.reset(new markov::WordsFrequency(freqs));
+    } catch (cereal::Exception& exc) {
+    }
+  }
 }
-
-template<class Archive>
-inline void save(Archive& archive, const markov::WordWithFrequency& chain) {
-  archive(*chain.word, chain.frequency);
-}
-
-template<class Archive>
-inline void load(Archive& archive, markov::Chain& inst) {
-  //archive(inst.head_);
-}
-
-template<class Archive>
-inline void load(Archive& archive, markov::WordsFrequency& inst) {
-  // for (auto& word : inst.freqs_) {
-  //archive(inst.freqs_);
-  //}
-}
-
-template<class Archive>
-inline void load(Archive& archive, markov::WordWithFrequency& chain) {
-  //archive(*chain.word, chain.frequency);
-}*/
+*/
 
 #endif // MARKOV_HPP_
